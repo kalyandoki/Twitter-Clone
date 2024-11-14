10327eefc7e2adc4c804d9dceb994c475d32a406
kalyandoki <kalyanreddy14343@gmail.com>
2024-11-14 15:39:13 +0530

    1-auth-controllers

--- Backend/routes/auth.routes.js
+++ Backend/routes/auth.routes.js
@@ -1,12 +1,17 @@
 import express from "express";
-import { signup, login, logout } from "../controllers/auth.controller.js";
+import {
+  getme,
+  signup,
+  login,
+  logout,
+} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 
 const rounter = express.Router();
 
+rounter.get("/me", protectRoute, getme);
 rounter.post("/signup", signup);
-
 rounter.post("/login", login);
-
 rounter.post("/logout", logout);
 
 export default rounter;
